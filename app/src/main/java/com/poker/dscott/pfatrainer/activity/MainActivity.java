bc900606787b7c15aed71745c22e8b178008748f
David Scott <dfsnow@gmail.com>
2015-09-13 00:27:37 -0400

    Basic working

--- app/src/main/java/com/poker/dscott/pfatrainer/activity/MainActivity.java
+++ app/src/main/java/com/poker/dscott/pfatrainer/activity/MainActivity.java
@@ -1,11 +1,16 @@
-package com.poker.dscott.pfatrainer;
+package com.poker.dscott.pfatrainer.activity;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
+import com.poker.dscott.pfatrainer.R;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,5 +38,13 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void createNewHand(View view) {
+
+        TextView textView = (TextView) findViewById(R.id.textView);
+        String text = "stuff";
+        textView.setText(text);
+        text = textView.getText().toString();
     }
 }
